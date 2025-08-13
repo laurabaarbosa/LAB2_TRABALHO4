19875f2e18222bb628a78c1875e2d082147b1eda
MARIANA DAUZACKER VAIANI CARNEIRO <C3007282@fgv.edu.br>
2025-08-13 13:56:49 -0300

    Terceiro commit: função de média com 7 números aleatórios

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ def cumprimento(texto):
 print(cumprimento("Laura da Cruz Barbosa"))
 
 
+import random
+
+def media_sete_numeros():
+    numeros = [random.randint(1, 100) for _ in range(7)]
+    media = sum(numeros) / len(numeros)
+    print("Números sorteados:", numeros)
+    return media
+
+print("Média dos 7 números:", media_sete_numeros())
